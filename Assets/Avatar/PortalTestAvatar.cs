f61fb79c7cc966789244100f707133a8e046ec7f
Lereldarion <131812197+lereldarion@users.noreply.github.com>
2025-06-23 20:33:22 +0200

    Improved test interaction
    Smooth grab of portal.
    Use Victory sign as handgun is hard to make on valve index.

--- Assets/Avatar/PortalTestAvatar.cs
+++ Assets/Avatar/PortalTestAvatar.cs
@@ -10,7 +10,6 @@ using UnityEngine;
 using VRC.SDK3.Avatars.Components;
 using VRC.SDK3.Dynamics.Constraint.Components;
 using VRC.SDK3.Avatars.ScriptableObjects;
-using Lereldarion.Portal;
 
 [assembly: ExportsPlugin(typeof(Lereldarion.PortalTestAvatarPlugin))]
 
@@ -27,7 +26,7 @@ namespace Lereldarion
 
         public GameObject SystemRoot;
         [Header("Portals")]
-        public VRCParentConstraint MenuPortal;
+        public VRCParentConstraint[] MenuPortals;
         public VRCParentConstraint LeftHandPortal;
         public VRCParentConstraint RightHandPortal;
     }
@@ -87,110 +86,111 @@ namespace Lereldarion
                 disabled.TransitionsTo(enabled).When(parameter.IsTrue());
                 enabled.TransitionsTo(disabled).When(parameter.IsFalse());
             }
+            for (int i = 0; i < config.MenuPortals.Length; i += 1)
             {
-                var layer = ctrl.NewLayer("Menu");
+                var layer = ctrl.NewLayer($"Menu{i}");
 
-                var parameter = layer.BoolParameter("Portal/Menu");
+                var parameter = layer.BoolParameter($"Portal/Menu{i}");
                 ma.NewParameter(parameter).WithDefaultValue(false).NotSaved();
-                new_installed_menu_item().Name("Menu Portal").Toggle(parameter);
+                new_installed_menu_item().Name($"Portal{i}").Toggle(parameter);
 
                 var disabled = layer.NewState("Disabled").WithAnimation(
                     aac.NewClip()
-                    .Scaling(config.MenuPortal.transform, Vector3.zero)
-                    .Animating(SetConstraintWorldFixed(config.MenuPortal, false))
+                    .Scaling(config.MenuPortals[i].transform, Vector3.zero)
+                    .Animating(SetConstraintWorldFixed(config.MenuPortals[i], false))
                 );
                 var enabled = layer.NewState("Enabled").WithAnimation(
                     aac.NewClip()
-                    .Scaling(config.MenuPortal.transform, Vector3.one)
-                    .Animating(SetConstraintWorldFixed(config.MenuPortal, true))
+                    .Scaling(config.MenuPortals[i].transform, Vector3.one)
+                    .Animating(SetConstraintWorldFixed(config.MenuPortals[i], true))
                 );
 
                 // Set scale to 0 for upload
-                config.MenuPortal.transform.localScale = Vector3.zero;
+                config.MenuPortals[i].transform.localScale = Vector3.zero;
 
+                disabled.DrivingLocally().Drives(parameter, false);
                 disabled.TransitionsTo(enabled).WithTransitionDurationSeconds(0.3f)
                     .When(parameter.IsTrue());
                 enabled.TransitionsTo(disabled).WithTransitionDurationSeconds(0.3f)
-                    .When(parameter.IsFalse());
+                    .When(parameter.IsFalse())
+                    .Or().When(layer.BoolParameter("Portal/System").IsFalse());
             }
 
+            void setup_hand_portal(string name, VRCParentConstraint anchor, System.Func<AacAv3, AacFlEnumIntParameter<AacAv3.Av3Gesture>> gesture)
             {
-                var layer = ctrl.NewLayer("Left Hand");
+                var layer = ctrl.NewLayer(name);
 
-                var contact = layer.BoolParameter("Portal/LeftHand/Contact");
+                var contact = layer.BoolParameter($"Portal/{name}/Contact");
+                var portal = anchor.transform.Find("Portal").GetComponent<VRCParentConstraint>();
 
                 var disabled = layer.NewState("Disabled").WithAnimation(
                     aac.NewClip()
-                    .Scaling(config.LeftHandPortal.transform, Vector3.zero)
-                    .Animating(SetConstraintWorldFixed(config.LeftHandPortal, false))
+                    .Scaling(portal.transform, Vector3.zero)
+                    .Animating(SetConstraintWorldFixed(anchor, false))
+                    .Animating(SetConstraintActiveSource(portal, 0))
+                    .Animating(SetConstraintWorldFixed(portal, false))
                 );
                 var in_hand = layer.NewState("In Hand").WithAnimation(
                     aac.NewClip()
-                    .Scaling(config.LeftHandPortal.transform, Vector3.one)
-                    .Animating(SetConstraintWorldFixed(config.LeftHandPortal, false))
+                    .Scaling(portal.transform, Vector3.one)
+                    .Animating(SetConstraintWorldFixed(anchor, false))
+                    .Animating(SetConstraintActiveSource(portal, 0))
+                    .Animating(SetConstraintWorldFixed(portal, false))
                 );
                 var world = layer.NewState("World").WithAnimation(
                     aac.NewClip()
-                    .Scaling(config.LeftHandPortal.transform, Vector3.one)
-                    .Animating(SetConstraintWorldFixed(config.LeftHandPortal, true))
+                    .Scaling(portal.transform, Vector3.one)
+                    .Animating(SetConstraintWorldFixed(anchor, true))
+                    .Animating(SetConstraintActiveSource(portal, 1))
+                    .Animating(SetConstraintWorldFixed(portal, true))
+                );
+                var world_to_hand = layer.NewState("World2Hand").WithAnimation(
+                    aac.NewClip()
+                    .Scaling(portal.transform, Vector3.one)
+                    .Animating(SetConstraintWorldFixed(anchor, true))
+                    .Animating(SetConstraintWorldFixed(portal, false))
+                    .Animating(edit =>
+                    {
+                        for (int i = 0; i < portal.Sources.Count; i += 1) {
+                            edit.Animates(portal, $"Sources.source{i}.Weight").WithSecondsUnit(curve => curve.Linear(0f, i == 0 ? 1 : 0).Linear(0.3f, i == 0 ? 0 : 1));
+                        }
+                    })
                 );
 
                 // TODO interrupt stuff or wait times to avoid spawning portal by error.
                 disabled.TransitionsTo(in_hand).WithTransitionDurationSeconds(0.3f)
-                    .When(layer.Av3().GestureLeft.IsEqualTo(AacAv3.Av3Gesture.HandGun));
+                    .When(gesture(layer.Av3()).IsEqualTo(AacAv3.Av3Gesture.Victory));
                 in_hand.TransitionsTo(disabled).WithTransitionDurationSeconds(0.3f)
-                    .When(layer.Av3().GestureLeft.IsEqualTo(AacAv3.Av3Gesture.Fist));
+                    .When(gesture(layer.Av3()).IsEqualTo(AacAv3.Av3Gesture.Fist));
 
-                in_hand.TransitionsTo(world).When(layer.Av3().GestureLeft.IsEqualTo(AacAv3.Av3Gesture.HandOpen));
-                // TODO smooth reparenting from world.
-                world.TransitionsTo(in_hand)
-                    .When(layer.Av3().GestureLeft.IsEqualTo(AacAv3.Av3Gesture.HandGun))
+                in_hand.TransitionsTo(world).When(gesture(layer.Av3()).IsEqualTo(AacAv3.Av3Gesture.HandOpen));
+                world.TransitionsTo(world_to_hand)
+                    .When(gesture(layer.Av3()).IsEqualTo(AacAv3.Av3Gesture.Victory))
                     .And(contact.IsTrue());
+                world_to_hand.AutomaticallyMovesTo(in_hand);
 
-                config.LeftHandPortal.transform.localScale = Vector3.zero;
+                portal.transform.localScale = Vector3.zero;
             }
-            {
-                var layer = ctrl.NewLayer("Right Hand");
-
-                var contact = layer.BoolParameter("Portal/RightHand/Contact");
-
-                var disabled = layer.NewState("Disabled").WithAnimation(
-                    aac.NewClip()
-                    .Scaling(config.RightHandPortal.transform, Vector3.zero)
-                    .Animating(SetConstraintWorldFixed(config.RightHandPortal, false))
-                );
-                var in_hand = layer.NewState("In Hand").WithAnimation(
-                    aac.NewClip()
-                    .Scaling(config.RightHandPortal.transform, Vector3.one)
-                    .Animating(SetConstraintWorldFixed(config.RightHandPortal, false))
-                );
-                var world = layer.NewState("World").WithAnimation(
-                    aac.NewClip()
-                    .Scaling(config.RightHandPortal.transform, Vector3.one)
-                    .Animating(SetConstraintWorldFixed(config.RightHandPortal, true))
-                );
-
-                // TODO copy improvements to left here
-
-                disabled.TransitionsTo(in_hand).WithTransitionDurationSeconds(0.3f)
-                    .When(layer.Av3().GestureRight.IsEqualTo(AacAv3.Av3Gesture.HandGun));
-                in_hand.TransitionsTo(disabled).WithTransitionDurationSeconds(0.3f)
-                    .When(layer.Av3().GestureRight.IsEqualTo(AacAv3.Av3Gesture.Fist));
-
-                in_hand.TransitionsTo(world).When(layer.Av3().GestureRight.IsEqualTo(AacAv3.Av3Gesture.HandOpen));
-                world.TransitionsTo(in_hand)
-                    .When(layer.Av3().GestureRight.IsEqualTo(AacAv3.Av3Gesture.HandGun))
-                    .And(contact.IsTrue());
-
-                config.RightHandPortal.transform.localScale = Vector3.zero;
-            }
+            setup_hand_portal("LeftHand", config.LeftHandPortal, av3 => av3.GestureLeft);
+            setup_hand_portal("RightHand", config.RightHandPortal, av3 => av3.GestureRight);
 
             ma.NewMergeAnimator(ctrl.AnimatorController, VRCAvatarDescriptor.AnimLayerType.FX);
             Object.DestroyImmediate(config);
         }
 
-        static private System.Action<AacFlEditClip> SetConstraintWorldFixed(VRCParentConstraint constraint, bool fixed_to_world) {
+        // Most constraint animations have only one source active at a time. Provide a quick setter for that.
+        static private System.Action<AacFlEditClip> SetConstraintActiveSource(VRCParentConstraint constraint, int active_source) {
             return clip => {
+                for (int i = 0; i < constraint.Sources.Count; i += 1) {
+                    clip.Animates(constraint, $"Sources.source{i}.Weight").WithOneFrame(i == active_source ? 1 : 0);
+                }
+            };
+        }
+
+        static private System.Action<AacFlEditClip> SetConstraintWorldFixed(VRCParentConstraint constraint, bool fixed_to_world)
+        {
+            return clip =>
+            {
                 clip.Animates(constraint, "FreezeToWorld").WithOneFrame(fixed_to_world ? 1 : 0);
             };
         }
